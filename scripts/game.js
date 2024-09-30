285a87b792a50dc24776f630d01067633f25c726
dor-greenwald <dorgreenwald@hilma.tech>
2024-09-30 17:17:57 +0300

    fix game

--- scripts/game.js
+++ scripts/game.js
@@ -181,56 +181,58 @@ function moveElement(event, x, y) {
 
 function move(objDigit, x, y, addx, addy) {
     clear();
-    layout[x][y] = 4; //empty
-    let objx = x + addx;
-    let objy = y + addy;
+    let objx;
+    let objy;
+    do {
 
-    //check if colided
-    switch (layout[objx][objy]) {
-        case 0:
-            addToScore(5);
-<<<<<<< HEAD
-            gameover()
-=======
-            won()
->>>>>>> 6a97a64ea54ee0f557fc6a36d6fde6ee3b5d03b7
+        layout[x][y] = 4; //empty
+        objx = x + addx;
+        objy = y + addy;
 
-            break;
+        //check if colided
+        switch (layout[objx][objy]) {
+            case 0:
+                addToScore(5);
+                won()
 
-        case 1:
-            objx = x;
-            objy = y;
-            break;
+                break;
 
-        case 6:
-        case 7:
-        case 8:
-        case 9:
-            if (objDigit >= 6) {
+            case 1:
                 objx = x;
                 objy = y;
-            }
-            else {
-                eatGhost(objx, objy);
-                addToScore(500);
-            }
-            break;
+                break;
 
-        case 3:
-            addToScore(100);
-            won()
-            break;
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                if (objDigit >= 6) {
+                    objx = x;
+                    objy = y;
+                }
+                else {
+                    eatGhost(objx, objy);
+                    addToScore(500);
+                }
+                break;
 
-        case 5:
-            if (objDigit >= 6) {
-                gameover();
-            }
+            case 3:
+                addToScore(100);
+                won()
+                break;
 
-    }
+            case 5:
+                if (objDigit >= 6) {
+                    gameover();
+                }
 
-    layout[objx][objy] = objDigit;
-    buildLayout();
+        }
 
+        layout[objx][objy] = objDigit;
+        buildLayout();
+
+
+    } while (objDigit >= 6);
     return [objx, objy];
 
 }
@@ -291,7 +293,7 @@ function eatGhost(x, y) {
 }
 
 function gameover() {
-    
+
     const imageGameOver = document.getElementsByClassName('gameover')[0];
     imageGameOver.style.height = '100vh';
     imageGameOver.style.width = '100vw';
@@ -341,16 +343,9 @@ function getValccurrence(array, value1, value2) {
     return count - 1;
 }
 
-<<<<<<< HEAD
-function Won() {
+function won() {
 
     if (getValccurrence(layout, 0, 3) === 0) {
-        clearInterval();
-=======
-function won(){
-    
-    if(getValccurrence(layout,0,3) === 0 ){
-        gameover()
->>>>>>> 6a97a64ea54ee0f557fc6a36d6fde6ee3b5d03b7
+        gameover();
     }
 }
